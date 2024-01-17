3203f6c32593afd28c8efb4d686710382472f628
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2024-01-17 20:21:23 +0600

    Add: route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,54 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import AdminDashboard from "../pages/admin/AdminDashboard";
+import CreateStudent from "../pages/admin/CreateStudent";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <AdminDashboard />,
+      },
+      {
+        path: "dashboard",
+        element: <AdminDashboard />,
+      },
+      {
+        path: "create-student",
+        element: <CreateStudent />,
+      },
+    ],
+  },
+
+  {
+    path: "login",
+    element: <Login />,
+  },
+
+  {
+    path: "/register",
+    element: <Register />,
   },
 ]);
 
