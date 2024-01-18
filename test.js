f556c754e962bbeaa7f0dc00089cf60a281c9db2
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2024-01-18 23:17:51 +0600

    Add: successfully login nad redux persist

--- test.js
+++ test.js
@@ -1,67 +1,78 @@
-export const adminPaths2 = [
-  {
-    name: "Dashboard",
-    path: "dashboard",
-    element: "AD",
-  },
-  {
-    name: "User Management",
-    children: [
-      {
-        name: "Create Admin",
-        path: "create-admin",
-        element: "CA",
-      },
-      {
-        name: "Create Faculty",
-        path: "create-faculty",
-        element: "CF",
-      },
-      {
-        name: "Create Student",
-        path: "create-student",
-        element: "CS",
-      },
-    ],
-  },
-];
+// export const adminPaths2 = [
+//   {
+//     name: "Dashboard",
+//     path: "dashboard",
+//     element: "AD",
+//   },
+//   {
+//     name: "User Management",
+//     children: [
+//       {
+//         name: "Create Admin",
+//         path: "create-admin",
+//         element: "CA",
+//       },
+//       {
+//         name: "Create Faculty",
+//         path: "create-faculty",
+//         element: "CF",
+//       },
+//       {
+//         name: "Create Student",
+//         path: "create-student",
+//         element: "CS",
+//       },
+//     ],
+//   },
+// ];
+
+// // const newArray = adminPaths2.reduce((acc, item) => {
+// //   if (item.path && item.element) {
+// //     acc.push({
+// //       path: item.path,
+// //       element: item.element,
+// //     });
+// //   }
+// //   if (item.children) {
+// //     item.children.forEach((child) => {
+// //       acc.push({
+// //         path: child.path,
+// //         element: child.element,
+// //       });
+// //     });
+// //   }
+// //   return acc;
+// // }, []);
 
 // const newArray = adminPaths2.reduce((acc, item) => {
-//   if (item.path && item.element) {
+//   if (item.path && item.name) {
 //     acc.push({
-//       path: item.path,
-//       element: item.element,
+//       key: item.name,
+//       label: "Nav_Link",
 //     });
 //   }
 //   if (item.children) {
-//     item.children.forEach((child) => {
-//       acc.push({
-//         path: child.path,
-//         element: child.element,
-//       });
+//     acc.push({
+//       key: item.name,
+//       label: item.name,
+//       children: item.children.map((child) => ({
+//         key: child.name,
+//         label: "Nav_link",
+//       })),
 //     });
 //   }
 //   return acc;
 // }, []);
 
-const newArray = adminPaths2.reduce((acc, item) => {
-  if (item.path && item.name) {
-    acc.push({
-      key: item.name,
-      label: "Nav_Link",
-    });
-  }
-  if (item.children) {
-    acc.push({
-      key: item.name,
-      label: item.name,
-      children: item.children.map((child) => ({
-        key: child.name,
-        label: "Nav_link",
-      })),
-    });
-  }
-  return acc;
-}, []);
+// console.log(newArray);
 
-console.log(newArray);
+const obj = {
+  name: "shahin",
+  role: "admin",
+  age: 18,
+  greet: () => {
+    return "Shahin";
+  },
+};
+
+console.log(JSON.stringify(obj));
