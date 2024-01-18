f10592da39158461096f4ec112a266d77053ecfc
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2024-01-18 18:51:15 +0600

    Add: faculty adn student routes

---
+++ src/pages/faculty/FacultyDashboard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FacultyDashboard = () => {
+  return <div>Faculty d</div>;
+};
+
+export default FacultyDashboard;
---
+++ src/pages/student/StudentDashboard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StudentDashboard = () => {
+  return <div>StudentDashboard StudentDashboard</div>;
+};
+
+export default StudentDashboard;
